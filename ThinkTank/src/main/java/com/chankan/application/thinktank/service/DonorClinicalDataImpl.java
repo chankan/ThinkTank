5802ba0dbf8df97d07b40afadb742f64edea9fa8
matrics9975 <mohitejitendra.123@gmail.com>
2015-01-31 16:06:26 +0530

    done

---
+++ ThinkTank/src/main/java/com/chankan/application/thinktank/service/DonorClinicalDataImpl.java
@@ -0,0 +1,35 @@
+package com.chankan.application.thinktank.service;
+
+import com.chankan.application.thinktank.bean.DonorClinicalDetails;
+import com.chankan.application.thinktank.bean.DonorDetails;
+import com.chankan.application.thinktank.dao.DonorClinicalDataDAO;
+import com.chankan.application.thinktank.dao.DonorMasterDAO;
+
+public class DonorClinicalDataImpl {
+	
+	
+
+	public DonorClinicalDetails UpdateAddClinicalData(DonorClinicalDetails clinicalData)
+	{
+		// TODO Auto-generated method stub
+		DonorClinicalDataDAO dao = new DonorClinicalDataDAO();
+		return dao.UpdateAddClinicalData(clinicalData);
+		
+		
+	}
+
+	public DonorClinicalDetails getDonorClinicalDataById(Integer donorId)
+	{
+		
+		DonorClinicalDataDAO dao = new DonorClinicalDataDAO();
+		DonorClinicalDetails getDoonor =  dao.getDonorClinicalDataById(donorId);
+		
+		return getDoonor;
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
