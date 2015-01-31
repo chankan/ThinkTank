9c187edc8a459d2359791f1ab9cf10bda81f93cc
codehalwai <kiran1892@yahoo.in>
2015-01-31 11:19:16 +0530

    try-catch in REST

--- ThinkTank/src/main/java/com/chankan/application/thinktank/rest/DonorResource.java
+++ ThinkTank/src/main/java/com/chankan/application/thinktank/rest/DonorResource.java
@@ -17,6 +17,7 @@ import javax.ws.rs.core.MediaType;
 import com.chankan.application.thinktank.bean.DonorCenter;
 import com.chankan.application.thinktank.bean.DonorDetails;
 import com.chankan.application.thinktank.bean.DonationTicketDetails;
+import com.chankan.application.thinktank.exception.ServiceException;
 
 
 @Path("/service")
@@ -30,51 +31,97 @@ public class DonorResource {
 	
 	
 	
-					/***Hospital URLs****/
+					/***Hospital URLs
+					 * @throws ServiceException ****/
 	@GET
 	@Path("/hospital")
-	public String getHospital() 
+	public String getHospital() throws ServiceException 
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceHospitalException");
+
+		}
 		return "Hello ThinkTank";
 	}
 		
-				/******DonorCenter URLs*******/
+				/******DonorCenter URLs
+				 * @throws ServiceException *******/
 	
 	@Path("/donorCenter")
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public List<DonorCenter> getDonorCenter()
+	public List<DonorCenter> getDonorCenter() throws ServiceException
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorCenterException");
+
+		}
 		return null;
 	}
 	
 	@Path("/donorCenter")
 	@POST
-	public DonorCenter updateDonorCenter()
+	public DonorCenter updateDonorCenter() throws ServiceException
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorCenterException");
+
+		}
 		return null;
 	}
 	
-					/****Donor URLs****/
+					/****Donor URLs
+					 * @throws ServiceException ****/
 	
 	@Path("/donorSearch/{donorId}")
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public DonorDetails getDonor(@PathParam("donorId") Integer donorId)
+	public DonorDetails getDonor(@PathParam("donorId") Integer donorId) throws ServiceException
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorDetailException");
+
+		}
+		
 		return donorList.get(1);
 	}
 	
 	@Path("/donor")
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public List<DonorDetails> getDonors()
+	public List<DonorDetails> getDonors() throws ServiceException
 	{
-		donor.setDonorDetailsID(1);
-		donor.setDonorName("Basanti");
-		donor.setDonorType("1");
+		try
+		{
+			
+			donor.setDonorDetailsID(1);
+			donor.setDonorName("Basanti");
+			donor.setDonorType("1");
+			
+			donorList.add(donor);
+
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorDetailException");
+		}
 		
-		donorList.add(donor);
 		
 		return donorList;
 	}
@@ -82,36 +129,66 @@ public class DonorResource {
 	@Path("/donor")
 	@POST
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public DonorDetails updateDonors()
+	public DonorDetails updateDonors() throws ServiceException
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorDetailException");
+
+		}
+		
 		return null;
 	}
 	
-						/***Ticket URLs****/
+						/***Ticket URLs
+						 * @throws ServiceException ****/
 	
 	
 	@Path("/ticket")
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public List<DonationTicketDetails> getTickets()
+	public List<DonationTicketDetails> getTickets() throws ServiceException
 	{
+		try
+		{
+			DonationTicketDetails ticket = new  DonationTicketDetails();
+			ticket.setTicketId(1);
+			ticket.setTicketRaisedBy("Rampur Hospital");
+			ticket.setTicketRaisedTo("Gabbar Singh");
+			ticket.setTicketCreationDate(new Date());
+			
+			ticketList.add(ticket);
+			
+			
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorDetailException");
+		}
 		
-		DonationTicketDetails ticket = new  DonationTicketDetails();
-		ticket.setTicketId(1);
-		ticket.setTicketRaisedBy("Rampur Hospital");
-		ticket.setTicketRaisedTo("Gabbar Singh");
-		ticket.setTicketCreationDate(new Date());
 		
-		ticketList.add(ticket);
-		return null;
+		
+		return ticketList;
 	}
 	
 	
 	@Path("/ticket")
 	@POST
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public DonationTicketDetails registerTicket()
+	public DonationTicketDetails registerTicket() throws ServiceException
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorTicketDetailException");
+
+		}
+		
 		return null;
 	}
 	
@@ -119,8 +196,17 @@ public class DonorResource {
 	@Path("/ticket/{ticketId}")
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public DonationTicketDetails getTicket(@PathParam("ticketId") Integer ticketId)
+	public DonationTicketDetails getTicket(@PathParam("ticketId") Integer ticketId) throws ServiceException
 	{
+		try
+		{
+				}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorTicketDetailException");
+			
+		}
+		
 		return ticketList.get(ticketId.intValue());
 	}
 	
@@ -129,8 +215,17 @@ public class DonorResource {
 	@Path("/ticketStatus")
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public DonationTicketDetails getTicketStatus()
+	public DonationTicketDetails getTicketStatus() throws ServiceException
 	{
+		try
+		{
+		}
+		catch(Exception serviceException)
+		{
+			throw new ServiceException("ServiceDonorTicketDetailException");
+
+		}
+		
 		return null;
 	}
 	
