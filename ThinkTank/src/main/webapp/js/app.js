77a3248e0298c02586e61ec5e50faeb970dac90f
chankan <chankan@users.noreply.github.com>
2015-01-31 14:56:35 +0530

    Added UI changes

--- ThinkTank/src/main/webapp/js/app.js
+++ ThinkTank/src/main/webapp/js/app.js
@@ -13,7 +13,7 @@ function getMap() {
 
 angular.module('project', [ 'ngSanitize', 'ngRoute', 'ngResource', 'mgcrea.ngStrap' ])
 .factory('TicketRes',[ '$resource', function($resource) {
-	var service = $resource('rest/service/ticket');
+	var service = $resource('rest/service/ticket/:ticketId',{ticketId: '@ticketId'});
 	return service;
 	} ])
 .config(function($routeProvider) {
@@ -30,7 +30,7 @@ angular.module('project', [ 'ngSanitize', 'ngRoute', 'ngResource', 'mgcrea.ngStr
 		controller : 'TicketCtrl',
 		templateUrl : 'view/ticket.html'
 	})
-	.when('/donarsearch', {
+	.when('/donorsearch/:ticketId', {
 		controller : 'SearchCtrl',
 		templateUrl : 'view/donorsearch.html'
 	})
@@ -47,11 +47,27 @@ angular.module('project', [ 'ngSanitize', 'ngRoute', 'ngResource', 'mgcrea.ngStr
 	};
 	$scope.get();
 })
-.controller('TicketCtrl', function($scope, $routeParams) {
-	var ticketId = $routeParams.ticketId;
+.controller('TicketCtrl', function($scope, $routeParams, TicketRes) {
+	$scope.ticketId = $routeParams.ticketId;
+	$scope.get=function(){
+		TicketRes.get({ticketId:$scope.ticketId},function(response){if(response){$scope.ticket=response;}}, function(){$scope.ticket=null;});
+	};
+	$scope.get();	
 })
-.controller('SearchCtrl', function($scope, $routeParams) {
-	var map=getMap();
+.controller('SearchCtrl', function($scope, $routeParams, TicketRes) {
+	$scope.map=getMap();
+	$scope.ticketId = $routeParams.ticketId;
+	$scope.get=function(){
+		TicketRes.get({ticketId:$scope.ticketId},function(response){if(response){$scope.ticket=response;}}, function(){$scope.ticket=null;});
+	};
+	$scope.get();	
+//	var myPopover = $popover(element, {title: 'My Title', content: 'My Content'});
+//	myPopover.$show();
+//	$scope.popover = {
+//			  "title": "Title",
+//			  "content": "Hello Popover<br />This is a multiline message!"
+//			};
+//	$scope.popover.$show();
 })
 
 
